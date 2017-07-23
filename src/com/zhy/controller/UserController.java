e058562a091cdb9e06e6f9a947ce1e59d7446cd9
Administrator <Administrator@WIN-K4SA6O8C8L5>
2017-07-23 10:04:41 +0800

    基于注解的数据类型转换

--- src/com/zhy/controller/UserController.java
+++ src/com/zhy/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.zhy.converter.DateEditor;
+import com.zhy.model.Time;
 import com.zhy.model.User;
 
 @Controller
@@ -65,5 +66,10 @@ public class UserController {
 		logger.info(user);
 		model.addAttribute("user", user);
 		return "success";
+	}
+	@RequestMapping(value="/test",method=RequestMethod.POST)
+	public String test(@ModelAttribute Time time,Model model) {
+		model.addAttribute("time", time);
+		return "success2";
 	}
 }
